924e9d088be84c49f733e73f53b8b68b125dfc80
Will Castelnau <will.castelnau@woodside.com.au>
2021-12-28 18:19:02 +0800

    Added trigger function on telemetry topic

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -27,5 +27,6 @@ module.exports = {
   rules: {
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
+    "max-len": [2, {"code": 120, "tabWidth": 4, "ignoreUrls": true}],
   },
 };
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,64 @@
 import * as functions from "firebase-functions";
+import * as express from "express";
+import {https} from "firebase-functions";
+import {PubSub} from "@google-cloud/pubsub";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const TELEMETRY_TOPIC = "telemetry";
+const REGION = "australia-southeast1";
+
+/**
+ * Handles inbound telemetry payloads, processes them and stores them as required
+ */
+exports.handleTelemetryPayload =
+    functions.region(REGION)
+        .pubsub.topic(TELEMETRY_TOPIC)
+        .onPublish((message) : Promise<void> => {
+          try {
+            const payload = JSON.stringify(message.json);
+            const attributes = JSON.stringify(message.attributes);
+            functions.logger.debug(`PubSub message was: ${payload}`);
+            functions.logger.debug(`Attributes are: ${attributes}`);
+
+            // Expected attributes
+            //  {
+            //      deviceId: "re-2.0-0000002",
+            //      deviceNumId: "3187732082407108",
+            //      deviceRegistryId: "RebelEspresso",
+            //      deviceRegistryLocation: "asia-east1",
+            //      projectId: "rebelthings",
+            //      subFolder: "telemetry"
+            //  }
+          } catch (e) {
+            functions.logger.error("PubSub message was not JSON, e");
+          }
+
+          return Promise.resolve();
+        });
+
+/**
+ * Used for testing so we can generate valid payloads
+ */
+exports.generateTestTelemetryPayload = functions.region(REGION)
+    .https
+    .onRequest((req: https.Request, resp: express.Response) : Promise<void> => {
+      const pubsub = new PubSub();
+
+      // Just make up a message
+      const data = Buffer.from(JSON.stringify( {
+        timestamp: 1640683082,
+        boiler_temp: 92,
+        boiler_setpoint: 119.0,
+        boiler_heat_duty: 5,
+        brew_temp: 67.5,
+      }));
+
+      pubsub.topic(TELEMETRY_TOPIC).publishMessage({data, json: true}).then((value) => {
+        console.log(value); // Success!
+        resp.sendStatus(200);
+      }).catch((reason) => {
+        functions.logger.error(`Failed to publish to topic ${TELEMETRY_TOPIC}`);
+        resp.sendStatus(500);
+      });
+
+      return Promise.resolve();
+    });
