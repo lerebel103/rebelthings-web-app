49260ed0f037d012ed09d8cc733e1671df70d129
Will Castelnau <will.castelnau@woodside.com.au>
2021-12-28 22:52:13 +0800

    Added storage of telemetry payloads to firestore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,38 @@
+import * as admin from "firebase-admin";
+admin.initializeApp();
+
 import * as functions from "firebase-functions";
 import * as express from "express";
 import {https} from "firebase-functions";
 import {PubSub} from "@google-cloud/pubsub";
+import {firestore} from "firebase-admin";
+import CollectionReference = firestore.CollectionReference;
+import DocumentData = firestore.DocumentData;
+import WriteResult = firestore.WriteResult;
 
 const TELEMETRY_TOPIC = "telemetry";
 const REGION = "australia-southeast1";
+const RETENTION_PERIOD_SEC = 7 * 24 * 60 * 60;
 
+
+/**
+ * Deletes old Firestore entries
+ *
+ * @param {CollectionReference<DocumentData>} db This is the reference to the db path to delete from.
+ */
+async function deleteOldEntries(db: CollectionReference<DocumentData>): Promise<WriteResult[]> {
+  // Now delete old entries
+  const batch = firestore().batch();
+  const epochNowSec = Date.now().valueOf() / 1000;
+  const oldestEpoch = epochNowSec - RETENTION_PERIOD_SEC;
+  const queryResult = await db.orderBy("timestamp").where("timestamp", "<=", oldestEpoch).get();
+  queryResult.forEach((entry) =>{
+    batch.delete(entry.ref);
+    console.log(`Deleting ${entry.data().timestamp}`);
+  });
+
+  return await batch.commit();
+}
 /**
  * Handles inbound telemetry payloads, processes them and stores them as required
  */
@@ -19,6 +46,22 @@ exports.handleTelemetryPayload =
             functions.logger.debug(`PubSub message was: ${payload}`);
             functions.logger.debug(`Attributes are: ${attributes}`);
 
+            const db = firestore()
+                .collection(message.attributes.deviceRegistryId)
+                .doc(message.attributes.deviceId)
+                .collection(`${TELEMETRY_TOPIC}`)
+                .doc(`${message.attributes.subFolder}`)
+                .collection("messages");
+            // Add new entry
+            db.doc(`${message.json.timestamp}`)
+                .set(message.json)
+                .then(() => {
+                  deleteOldEntries(db).then( () =>{
+                    return Promise.resolve();
+                  });
+                }).catch((reason) => {
+                  return Promise.reject(reason);
+                });
             // Expected attributes
             //  {
             //      deviceId: "re-2.0-0000002",
@@ -29,11 +72,12 @@ exports.handleTelemetryPayload =
             //      subFolder: "telemetry"
             //  }
           } catch (e) {
-            functions.logger.error("PubSub message was not JSON, e");
+            functions.logger.error(`Failed to write payload to Firestore, ${e}`);
           }
 
           return Promise.resolve();
         });
+
 
 /**
  * Used for testing so we can generate valid payloads
@@ -43,22 +87,32 @@ exports.generateTestTelemetryPayload = functions.region(REGION)
     .onRequest((req: https.Request, resp: express.Response) : Promise<void> => {
       const pubsub = new PubSub();
 
+      const attributes = {
+        deviceId: "re-2.0-0000002",
+        deviceNumId: "3187732082407108",
+        deviceRegistryId: "RebelEspresso",
+        deviceRegistryLocation: "asia-east1",
+        projectId: "rebelthings",
+        subFolder: "telemetry",
+      };
       // Just make up a message
       const data = Buffer.from(JSON.stringify( {
-        timestamp: 1640683082,
+        timestamp: Date.now().valueOf() / 1000,
         boiler_temp: 92,
         boiler_setpoint: 119.0,
         boiler_heat_duty: 5,
         brew_temp: 67.5,
       }));
 
-      pubsub.topic(TELEMETRY_TOPIC).publishMessage({data, json: true}).then((value) => {
+      pubsub.topic(TELEMETRY_TOPIC).publishMessage({data, attributes, json: true}).then((value) => {
         console.log(value); // Success!
         resp.sendStatus(200);
       }).catch((reason) => {
-        functions.logger.error(`Failed to publish to topic ${TELEMETRY_TOPIC}`);
+        functions.logger.error(`Failed to publish to topic ${TELEMETRY_TOPIC}, ${reason}`);
         resp.sendStatus(500);
       });
 
       return Promise.resolve();
     });
+
+
